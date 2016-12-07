11e9428d7826096e670a644915d37a7b7587c8f0
Avery Pratt <apratt91@gmail.com>
2016-12-06 16:25:39 -0800

    refactored test functions and got __name__ == __main__ section to print to the console.

--- src/series.py
+++ src/series.py
@@ -1,25 +1,57 @@
+"""This module defines functions that implement mathematical series."""
+
 def fibonacci(n):
-    start = 0
-    second = 1
-    if n == 1:
-        return start
-    elif n == 2:
-        return second
-    else:
-        for num in range(n - 2):
-            third = start + second
-            start = second
-            second = third
-        return second
+	"""Returns the nth number of the Fibonacci sequence (starting with 0, 1)"""
+	first = 0
+	second = 1
+	if n == 1:
+	    return first
+	elif n == 2:
+	    return second
+	else:
+	    for num in range(n - 2):
+	        third = first + second
+	        first = second
+	        second = third
+	    return second
 
 
-def lucas(n, m=2, o=1):
-    if n <= 1:
-        return m
-    return lucas(n - 1, o, o + m)
+def lucas(n):
+	"""Returns the nth number of the Lucas sequence (starting with 2, 1)"""
+	first = 2
+	second = 1
+	if n == 1:
+	    return first
+	elif n == 2:
+	    return second
+	else:
+	    for num in range(n - 2):
+	        third = first + second
+	        first = second
+	        second = third
+	    return second
 
 
-def sum_series(n, m=0, o=1):
-    if n <= 1:
-        return m
-    return sum_series(n - 1, o, o + m)
+def sum_series(n, first=0, second=1):
+	"""Returns the nth number of the Fibonacci sequence, or a different sequence starting with the parameters given"""
+	if n <= 1:
+	    return first
+	return sum_series(n - 1, second, second + first)
+
+if __name__ == "__main__":
+	print()
+	print(__doc__)
+	print("...")
+	print()
+	print("fibonacci(n):")
+	print()
+	print("    " + fibonacci.__doc__)
+	print()
+	print("lucas(n):")
+	print()
+	print("    " + lucas.__doc__)
+	print()
+	print("sum_series(n):")
+	print()
+	print("    " + sum_series.__doc__)
+	print()
